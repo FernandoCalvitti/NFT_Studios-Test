b907283f765ab49e5e527bf133af3c287ca4b73e
FernandoCalvitti <federicofernandogiribaldi@hotmail.com>
2021-10-11 12:55:08 -0300

    feat(spinner): created spinner component and added initial state loading

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Item from '../Item/Item'
 import fetchApi from '../../API/apiCall'
+import Spinner from '../Spinner/Spinner'
 
 
 //Component that contains items
@@ -8,28 +9,37 @@ const ItemList = () => {
 
     //Elements to render
     const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     //Fetch on the first render 
-    useEffect(() => {        
-        fetchApi().then(res => setItems(res.assets))
-        console.log("rendered");
-    },[])
+    useEffect(() => {   
+        setIsLoading(true)
+        fetchApi().then(res => {
+            setItems(res.assets)
+            setIsLoading(false)  
+        })
+    }, [])
 
     //Renders a component for every Item
     return (
-        <div className="container">
-            <div className="ItemList row">
-                {items.map(item => {
-                    return (
-                        <Item 
-                            id={item.id}
-                            image={item.image_preview_url}
-                            key={item.id}
-                            name={item.name}
-                        />
-                    )
-                })}
-            </div>
+        <div>
+            { isLoading ?
+                <Spinner /> :
+                <div className="container">
+                    <div className="ItemList row">
+                        {items.map(item => {
+                            return (
+                                <Item 
+                                    id={item.id}
+                                    image={item.image_preview_url}
+                                    key={item.id}
+                                    name={item.name}
+                                />
+                            )
+                        })}
+                    </div>
+                </div>
+            }
         </div>
     )
 }
