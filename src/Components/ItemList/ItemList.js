16f2896427ad0f7a161cf6671933466e054ecb1e
FernandoCalvitti <federicofernandogiribaldi@hotmail.com>
2021-10-11 21:15:05 -0300

    feat(useCallToApi): added custom hook to extract logic from itemlist component

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,40 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useRef } from 'react'
 import Item from '../Item/Item'
-import fetchApi from '../../API/apiCall'
 import Spinner from '../Spinner/Spinner'
+import useCallToApi from '../../hooks/useCallToApi'
 
 
 //Component that contains items
 const ItemList = () => {
 
-    //Elements to render
-    const [items, setItems] = useState([])
-
-    //Is loading
-    const [isLoading, setIsLoading] = useState(false)
-
-    //State for new blocks of images
-    const [blocks, setBlocks] = useState(20)
-    
-    //Add more img to the list
-    const loadMore = async (block) => {
-        setIsLoading(true)
-        block = blocks
-        const res = await fetchApi(block)
-            .then(setBlocks(blocks + 20))
-            const moreItems = res.assets
-        setItems([...items, ...moreItems])
-        setIsLoading(false)
-    }
-
-    //Fetch on the first render 
-    useEffect(() => {   
-        setIsLoading(true)
-        fetchApi(0).then(res => {
-            setItems(res.assets)
-            setIsLoading(false)
-        })
-    }, [])
+    //Call to hook
+    const { isLoading, loadMore, items } = useCallToApi()
 
     //Renders a component for every Item
     return (
@@ -53,8 +27,7 @@ const ItemList = () => {
                                         name={item.name}
                                     />
                                 )
-                            })}
-                            {(isLoading && items.length !== 0) ? <Spinner /> : null}
+                            })}                            
                         </div>
                     </div>
                 }
