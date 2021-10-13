2c8d62789e44fb4821a7f2466551ebd9abb4bad1
FernandoCalvitti <federicofernandogiribaldi@hotmail.com>
2021-10-13 14:18:04 -0300

    feat(Header): added a header to the layout

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="btn-secondary p-4">
+            <h1 >
+                NFT STUDIOS CHALLENGE
+            </h1>
+            <strong>2021</strong>
+        </div>
+    )
+}
+
+export default Header
