105e65d6bf11603e89e53c7a0b7dfcc4285118a9
FernandoCalvitti <federicofernandogiribaldi@hotmail.com>
2021-10-13 19:30:13 -0300

    feat(snapshots): created a snapshot folder to test layout component render

--- src/Tests/Item.test.js
+++ src/Tests/Item.test.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Item from "../Components/Item/Item";
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 describe('Item tests', () => {
 
-    test('Item is rendered', () => {
+    //Creates a render before each test
+
+    let item;
+
+    beforeEach(() => {
 
         const props = {
             image: 'https://lh3.googleusercontent.com/wTb-RjIladuxVUtOixNrTjW1XdA5cUSOFkA7F0r3xc3wX_JdkzBaHJ6PqO_BH9KNn85_h6t4tOkEoBXFYGQ4BNf8gF52M7Pky27DpA=s250',
@@ -12,7 +16,10 @@ describe('Item tests', () => {
             id: 71482076
         }
 
-        const item = render(<Item name={props.name} image={props.image} id={props.id}/>)
+        item = render(<Item name={props.name} image={props.image} id={props.id}/>)
+    })
+
+    test('Item is rendered', () => {
     
         expect(item.getByTestId("item-alone")).toBeInTheDocument()
 
@@ -20,18 +27,8 @@ describe('Item tests', () => {
     
     
     test('Text rendered is "Baby Yoda"', () => {
-
-        const props = {
-            image: 'https://lh3.googleusercontent.com/wTb-RjIladuxVUtOixNrTjW1XdA5cUSOFkA7F0r3xc3wX_JdkzBaHJ6PqO_BH9KNn85_h6t4tOkEoBXFYGQ4BNf8gF52M7Pky27DpA=s250',
-            name: 'Baby Yoda',
-            id: 71482076
-        }
-
-        const item = render(<Item name={props.name} image={props.image} id={props.id}/>)
     
-        item.getByText(props.name)
-    })
-
-    
+        item.getByText("Baby Yoda")
+    })    
 
 })
